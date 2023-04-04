60f31e5dc6ab5e4a06113d240c4468650f8d0404
tradingsensei <tradingsensei21@gmail.com>
2023-04-04 13:00:12 +0200

    Add API credentials

--- SmileBot.py
+++ SmileBot.py
@@ -1,9 +1,11 @@
+#Bot for @QueeCrypto account!
+
 from sys import api_version
 import tweepy
 
 #Authenticate Twitter details
-auth = tweepy.OAuthHandler = ("CONSUMER_KEY" , "CONSUMER_SECRET")
-auth.login_cred.OAuthHandler = ("ACCESS_TOKEN" , "ACCESS_SECRET")
+auth = tweepy.OAuthHandler = ("OJ4vSsRtkHVcRwjgRQ6iJwaPJ" , "Z5QUxuNZn6GW5or9Hk2nazTHIvMs37eHHmgRpcgBkOYL8OljIZ")
+auth.login_cred.OAuthHandler = ("1397504480816648196-P6jfjb3MolMxbncAXIfxTysTnd3TMp" , "bwdFpBoZP0boX5MezDgYBXGtAjfWYpEMyUH2S99gQTVlJ")
 
 #Create API object
 auth = tweepy.API(auth)
