aae7797d223cc4ddcdfcac063a750186673d2cb5
tradingsensei <tradingsensei21@gmail.com>
2023-04-04 13:54:50 +0200

    Add promo content

--- SmileBot.py
+++ SmileBot.py
@@ -3,12 +3,34 @@
 from sys import api_version
 import tweepy
 
+consumer_key="OJ4vSsRtkHVcRwjgRQ6iJwaPJ"
+consumer_secret="Z5QUxuNZn6GW5or9Hk2nazTHIvMs37eHHmgRpcgBkOYL8OljIZ"
+access_token="MmVlTEMyRmFTbGR0TGszXzZEbHg6MTpjaQ"  #v2 OAuth
+access_token_secret="hQCp-WGFF0TuPVt1yiV1EhVYxxqWRhMKumy1YCVDJIVe3rKo3r" #v2 OAuth
+
 #Authenticate Twitter details
-auth = tweepy.OAuthHandler = ("OJ4vSsRtkHVcRwjgRQ6iJwaPJ" , "Z5QUxuNZn6GW5or9Hk2nazTHIvMs37eHHmgRpcgBkOYL8OljIZ")
-auth.login_cred.OAuthHandler = ("1397504480816648196-P6jfjb3MolMxbncAXIfxTysTnd3TMp" , "bwdFpBoZP0boX5MezDgYBXGtAjfWYpEMyUH2S99gQTVlJ")
+auth = tweepy.OAuthHandler = (consumer_key , consumer_secret)
+auth.login_cred.OAuthHandler = (access_token , access_token_secret)
 
 #Create API object
 auth = tweepy.API(auth)
 
 #Create a tweet
-api_version.status_update("Hello, I am back. I have missed you")
+
+#api_version.status_update("Hello, I am back. I have missed you")
+
+api_version.status_update("Name one awesome thing you will do today😎 '\n'Beside following @tradingsensei 👀 '\n''\n' `$link $VRA $btc $ada $dot $xrp  $SXP $bnb $atom $eth $ltc $kcs $aioz $theta $tfuel")
+
+api_version.status_update("Name one awesome thing you will do today😎 '\n'Beside following @tradingsensei 👀 '\n''\n'  $STORY $celo $fil $eros $dino $xrp $mdx $zora $zoo $neo $doge $btc $sol $btcxec $eth $dai $qtum $pngn $mbox $bnb $eth")
+
+api_version.status_update("Name one awesome thing you will do today😎 '\n'Beside following @tradingsensei 👀  '\n''\n'  $luna $rsr $bsc $rune $hnt $nano $reef $tel $kin $SXP $ $dash $knc $bnb $bat $doge $aave $xtz $xlm $xrp")
+
+api_version.status_update("Name one awesome thing you will do today 😎 '\n'Beside following @tradingsensei 👀 '\n''\n'  $BTC $CHZ $ONT $XRP #ATH $MATIC $LTC $HOT #bsc $LOL $SXP $QTUM $DIA $COTI $FET $ROSE $CAKE")
+
+api_version.status_update("Name one awesome thing you will do today 😎 '\n'Beside following @tradingsensei 👀 '\n''\n'  $BTC $ETH $CRV  $TP $USDC $bsc $SKL $SXP $HOT $ANKR $XVG $dot #SXP $ATH $SL $SAND $BTT $LTC $USDT $REAU $STC")
+
+api_version.status_update("Name one awesome thing you will do today 😎 '\n'Beside following @tradingsensei 👀 '\n''\n'  $SPI $DOT $GRT $VSP $BTT $THETA #bsc  $SAND $CHZ $SPI $ONE $USDT $GLD $SLV $ABNB $bsc $SPOT")
+
+api_version.status_update("Name one awesome thing you will do today 😎 '\n'Beside following @tradingsensei 👀 '\n''\n' $BTC $ADA $ETH $SL $BNB $HYDRA $SOL $dot $LOL $IT  $BART $TP $BSC $DCB $SXP $H $LTC $DOT $PT $RSI $ATH")
+
+#api_version.status_update("Name one awesome thing you will do today '\n'Beside following @tradingsensei'\n''\n' $yongs `$link $VRA $btc $ada $dot $xrp  $SXP $bnb $atom $eth $ltc $kcs $aioz $theta $tfuel")
